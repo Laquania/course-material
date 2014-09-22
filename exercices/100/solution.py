5dc133d0b6ff12bdb3d1a374a276db5e60ee3e2d
Laquania <amaury.monmeyran@gmail.com>
2014-09-22 18:21:21 +0200

    .

--- exercices/100/solution.py
+++ exercices/100/solution.py
@@ -5,3 +5,13 @@ Created on Mon Sep 22 17:27:09 2014
 @author: Amaury
 """
 
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+
+for element in ('latitude','longitude','number','name','address'):
+    print(element,station[element])
